23cc87453b96b76918bae6b7e79e9b9797fb7570
Frac7 <francescacella96@gmail.com>
2020-10-03 16:52:05 +0200

    Updates

---
+++ precache-manifest.b1a1bd9f11ca830b15f812d78b4be8d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a45cf6e952895f5105f3d10002d7799",
+    "url": "/Shifts/index.html"
+  },
+  {
+    "revision": "73034c3b3a53d6945008",
+    "url": "/Shifts/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8c4e3509524b855fadc2",
+    "url": "/Shifts/static/css/main.dda5870c.chunk.css"
+  },
+  {
+    "revision": "73034c3b3a53d6945008",
+    "url": "/Shifts/static/js/2.4ae65207.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Shifts/static/js/2.4ae65207.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c4e3509524b855fadc2",
+    "url": "/Shifts/static/js/main.445a9eac.chunk.js"
+  },
+  {
+    "revision": "7fd8495946620c2ced0a",
+    "url": "/Shifts/static/js/runtime-main.ce294470.js"
+  }
+]);
