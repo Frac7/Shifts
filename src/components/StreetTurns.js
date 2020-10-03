94ebbc96c4abaebd7f104a8a063ad6e5c2401d75
Frac7 <francescacella96@gmail.com>
2020-10-03 14:50:41 +0200

    Street turns component refactored

--- src/components/StreetTurns.js
+++ src/components/StreetTurns.js
@@ -1,55 +1,41 @@
-import React from 'react';
-import data from '../data/data.json';
+import React, { useMemo } from 'react';
+import data from '../data';
 
-const totalDays = 7;
+const StreetTurns = ({ date }) => {
+	const selectedDate = useMemo(() => new Date(date), [date]);
+	const selectedDay = useMemo(() => selectedDate.getDay() - 1, [selectedDate]);
 
-const streetTurns = data.streetTurns;
-const days = streetTurns.map(function (el) {
-	return el.day;
-});
+	const streetTurns = useMemo(() => data.streetTurns, []);
+	const days = useMemo(() => streetTurns.map((el) => el.day), [streetTurns]);
+	const turnIndex = useMemo(() => days.indexOf(selectedDay), [days, selectedDay]);
 
-class StreetTurns extends React.Component {
-	render() {
-		let selectedDate = new Date(this.props.date);
-		let selectedDay = selectedDate.getDay() - 1;
+	const monthInitialDay = useMemo(() =>
+		new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1).getDay() - 1,
+		[selectedDate]);
+	const firstChosenDay = useMemo(() => monthInitialDay > selectedDay ?
+		7 - monthInitialDay + selectedDay + 1 :
+		selectedDay - monthInitialDay + 1, [monthInitialDay, selectedDay]);
+	const numberOfTheMonth = useMemo(() =>
+		turnIndex !== -1 && firstChosenDay + (7 * streetTurns[turnIndex].week),
+		[firstChosenDay, streetTurns, turnIndex]);
 
-		let turnIndex = days.indexOf(selectedDay);
-
-		let html = <p className="lead"> - </p>;
-
-		if(turnIndex !== -1) { //se il giorno della settimana è presente nell'array dei giorni
-			//primo giorno del mese in assoluto
-			let monthInitialDate = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1);
-			let monthInitialDay = monthInitialDate.getDay() - 1;
-
-			//calcolo dell'offset tra il giorno scelto e il primo giorno del mese
-			//si ottiene il numero del giorno scelto, il primo che cade in quel mese
-			let firstChoosenDay = monthInitialDay > selectedDay? totalDays - monthInitialDay + selectedDay + 1 :
-				selectedDay - monthInitialDay + 1;
-
-			//numero del giorno scelto durante la settimana in cui passa la lavastrada
-			let numberOfTheMonth = firstChoosenDay + (totalDays * streetTurns[turnIndex].week);
-
-			if(numberOfTheMonth === selectedDate.getDate()) {
-				html = <p className="h3">
-					<span className="badge badge-secondary street-turns-badge">{streetTurns[turnIndex].street}</span>
-				</p>;
-			}
-		}
-
-		return(
-			<div className="container jumbotron street-turns">
-				<div className="row justify-content-center align-items-center">
-					<div className="col-md-4">
-						<p className="lead">Turno della lavastrada: </p>
-					</div>
-					<div className="col-md-6">
-						{html}
-					</div>
+	return(
+		<div className="container jumbotron street-turns">
+			<div className="row justify-content-center align-items-center">
+				<div className="col-md-4">
+					<p className="lead">Turno della lavastrada: </p>
+				</div>
+				<div className="col-md-6">
+					{turnIndex !== -1 && numberOfTheMonth === selectedDate.getDate() ?
+						<p className="h3">
+							<span className="badge badge-secondary street-turns-badge">{streetTurns[turnIndex].street}</span>
+						</p> :
+						<p className="lead"> - </p>
+					}
 				</div>
 			</div>
-		)
-	}
+		</div>
+	);
 }
 
 export default StreetTurns;
